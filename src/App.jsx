a99499adfaa1fd93441d783adb7e666d0cc9f5bc
RuidoLAB <contactonicco@gmail.com>
2026-05-22 11:34:08 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Trips from './pages/Trips'
 import Dashboard from './pages/Dashboard'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Trips />} />
+        <Route path="/trip/:id" element={<Dashboard />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
